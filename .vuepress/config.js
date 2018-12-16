2561f9cc6fe85e605190f4f5c9a2b4847180dc8f
Austin Casteel <austin.casteel@gmail.com>
2018-12-16 17:19:09 -0500

    got to love dyslexia

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -201,7 +201,7 @@ module.exports = {
             title: 'Plugins',
             collapsable: false,
             children: [
-              'plugins/marrytts-setup',
+              'plugins/marytts-setup',
               'plugins/pocketsphinx-install',
               'plugins/deepspeech-setup'
             ]
