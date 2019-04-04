55ad2439b339770d0ee6ad017602738500de0447
Austin Casteel <austin.casteel@gmail.com>
2019-04-03 23:09:03 -0400

    Fixed thanks to Aaron

--- .vuepress/components/DocPreviousVersions.vue
+++ .vuepress/components/DocPreviousVersions.vue
@@ -69,7 +69,7 @@
 export default {
   data () {
     return {
-      versionNumbers: ['stable', 'dev', 'dev.1'],
+      versionNumbers: ['stable', 'dev'],
       currentVersion: 'stable',
       open: false
     }
@@ -80,19 +80,19 @@ export default {
     }
   },
   created () {
-    if (this.$site.base && this.$site.base.indexOf('dev') > 0) {
-      this.currentVersion = this.$site.base.replace('dev', '').replace(/\//g, '')
+    this.pagepath = this.$page.path
+    if (this.$page.path && this.$page.path.indexOf('/dev/') == 0) {
+      this.pagepath = this.pagepath.replace(/^.{4}/,'')
+      this.currentVersion = 'dev'
       this.versionNumbers = ['stable', this.currentVersion]
     }
   },
   computed: {
     versions () {
       return this.versionNumbers.map(version => {
-        let url = this.$page.path
-        if (version === 'stable') {
-          url = 'https://projectnaomi.com' + url
-        } else {
-          url = 'https://projectnaomi.com' + '/' + version + url
+        let url = this.pagepath
+        if (version != 'stable') {
+          url = '/' + version + url
         }
         return {
           number: version,
@@ -103,109 +103,3 @@ export default {
   }
 }
 </script>
-
-<!--<template>
-  <div class="page-versions">
-    <div class="dropdown-wrapper" :class="{ open }">
-      <a class="dropdown-title" @click="toggle">
-        <span class="title">Version: {{currentVersion}}</span>
-        <span class="arrow" :class="open ? 'down' : 'right'"></span></span>
-      </a>
-      <ul class="nav-dropdown" v-show="open">
-        <li v-for="version in versions" class="dropdown-item">
-          <a class="current" v-if="version.release === currentVersion">{{version.release}}</a>
-          <a v-else :href="version.url">{{version.release}}</a>
-        </li>
-      </ul>
-    </div>
-  </div>
-</template>
-
-<style lang="stylus">
-.content:not(.custom)>h1:first-child
-  margin-right 140px
-.page-versions
-  position absolute
-  top 5rem
-  white-space nowrap
-  right 2rem
-  font-size 9pt
-  margin 5px
-  padding 3px
-  height 14px
-  user-select none
-  .dropdown-wrapper
-    padding 2px
-    position absolute
-    right 0
-  .dropdown-title
-    border 1px solid #eee
-    border-radius 2px
-    padding 5px
-    color black
-    cursor pointer
-    &:hover
-      text-decoration none !important
-      border 1px solid #ccc
-  .nav-dropdown
-    box-sizing border-box
-    max-height calc(100vh - 2.7rem)
-    overflow-y auto
-    top 100%
-    right 0
-    background-color #fff
-    padding 10px 0
-    border 1px solid #ddd
-    border-bottom-color #ccc
-    text-align left
-    border-radius 0.25rem
-    white-space nowrap
-    margin 0;
-    position relative
-    top 0
-    .current
-      color black
-      font-weight bold !important
-      &:hover
-        text-decoration none
-        color black !important
-</style>
-
-<script>
-export default {
-  data () {
-    return {
-      versionReleases: ['stable', 'dev'],
-      currentVersion: 'stable',
-      open: false
-    }
-  },
-  methods: {
-    toggle () {
-      this.open = !this.open
-    }
-  },
-  created () {
-    if (this.$site.base && this.$site.base.indexOf('dev') > 0) {
-      this.currentVersion = this.$site.base.replace('dev', '').replace(/\//g, '')
-      this.versionReleases = ['stable', this.currentVersion] // to avoid having to regenerate previous sites
-    }
-  },
-  computed: {
-    versions () {
-      return this.versionReleases.map(version => {
-        let url = this.$page.path
-        if (version === 'stable') {
-          url = '/' + url
-        } else if (version === 'dev') {
-          url = '/' + version + url
-        }
-        return {
-          release: version,
-          url: url
-        }
-      })
-    }
-  }
-}
-</script>-->
