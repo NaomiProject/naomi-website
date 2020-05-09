363a0a3e382f83bea52e47f03158eb0e20ae0c77
Austin Casteel <austin.casteel@gmail.com>
2020-05-09 13:48:09 -0500

    Automating NPE with Naomi-Plugins Repo

--- prepare-docs.rb
+++ prepare-docs.rb
@@ -1,47 +1,77 @@
-# This will clone https://github.com/naomiproject/naomi-docs
-# and migrate content into the website with some changes
-
 require "fileutils"
-require "net/http"
-require "uri"
-require "rexml/document"
-# require "nokogiri"
+require "csv"
 
-$master_docs_repo = "https://github.com/naomiproject/naomi-docs"
-$master_docs_repo_root = $master_docs_repo + "/blob/master"
-$dev_docs_repo = "https://github.com/naomiproject/naomi-docs"
-$dev_docs_repo_root = $dev_docs_repo + "/blob/dev"
-$version = nil
-
-if ENV["N_DOCS_VERSION"] then
-    puts ">>> Generating docs for version #{ENV["N_DOCS_VERSION"]}"
-    $version = ENV["N_DOCS_VERSION"]
-    $version += ".0" if $version.split(".").length == 2
-end
-
-if (ARGV[0] && ARGV[0] == "--no-clone" && Dir.exists?("docs")) then
-    puts ">>> Re-using existing clone"
+puts ">>> Reading docs repo"
+if (Dir.exists?("docs")) then
+    puts " -> Re-using existing clone"
 else
-    puts ">>> Deleting docs if existing..."
+    puts " -> Deleting docs if existing..."
     FileUtils.rm_rf("docs")
 
-    puts ">>> Cloning naomi-docs"
-    `git clone --branch master https://github.com/naomiproject/naomi-docs docs`
+    puts " -> Cloning naomi-docs"
+    `git clone -q --branch master https://github.com/naomiproject/naomi-docs docs`
 end
 
-if (ARGV[0] && ARGV[0] == "--no-clone" && Dir.exists?("dev")) then
-    puts ">>> Re-using existing clone"
+puts ">>> Reading dev docs repo"
+if (Dir.exists?("dev")) then
+    puts " -> Re-using existing clone"
 else
-    puts ">>> Deleting dev if existing..."
+    puts " -> Deleting dev if existing..."
     FileUtils.rm_rf("dev")
 
-    puts ">>> Cloning naomi-docs_dev"
-    `git clone --branch dev https://github.com/naomiproject/naomi-docs dev/docs`
+    puts " -> Cloning naomi-docs_dev"
+    `git clone -q --branch dev https://github.com/naomiproject/naomi-docs dev/docs`
 end
+
+puts ">>> Reading plugins repo"
+if (Dir.exists?("naomi-plugins")) then
+    puts " -> Re-using existing clone"
+else
+    puts " -> Deleting docs if existing..."
+    FileUtils.rm_rf("naomi-plugins")
+
+    puts " -> Cloning naomi-plugins"
+    `git clone -q --branch master https://github.com/naomiproject/naomi-plugins naomi-plugins`
+end
+
+puts ">>> Reading plugins.csv"
+file_path = "naomi-plugins/plugins.csv"
+csv = CSV.read(file_path, :headers=>true)
+
+puts " -> Deleting naomi-plugin-downloads if existing..."
+FileUtils.rm_rf("naomi-plugin-downloads")
+FileUtils.mkdir_p("naomi-plugin-downloads")
+csv['Repository'].each do |a|
+    puts " -> Cloning #{a}"
+    `git -C ./naomi-plugin-downloads clone -q #{a}`
+end
+
+Dir.glob("naomi-plugin-downloads/**") { |path|
+    plugin = File.basename(path)
+    puts " --> #{plugin}"
+    File.open ('naomi-plugin-downloads/' + plugin + '/readme.md') do |file|
+        file.find do |line|
+            if line =~ /type:/
+                line_clean = line.split("type: ").join('').strip
+                puts " ---> #{line_clean}"
+                FileUtils.mkdir_p("dev/docs/NPE-Files/_plugins_#{line_clean}/#{plugin}")
+                FileUtils.cp("naomi-plugin-downloads/" + plugin + "/readme.md", "dev/docs/NPE-Files/_plugins_#{line_clean}/#{plugin}")
+            end
+            if line =~ /logo:/
+                logo_clean = line.split("logo: ").join('').strip
+                logo_clean2 = logo_clean.split("images/plugins/").join('')
+                puts " ---> #{logo_clean2}"
+                FileUtils.cp("naomi-plugin-downloads/#{plugin}/#{logo_clean}", "dev/docs/images/plugins")
+            end
+        end
+    end
+}
 
 puts ">>> Migrating the introduction article"
 #FileUtils.mv("docs/introduction.md", "docs/readme.md")
+FileUtils.mv("dev/docs/readme.md", "dev/docs/developer/website/index.md")
 FileUtils.mv("dev/docs/introduction.md", "dev/docs/readme.md")
+FileUtils.mv("README.md", "dev/docs/developer/docs/index.md")
 FileUtils.mv("introduction.md", "README.md")
 
 puts ">>> Migrating logos"
@@ -49,78 +79,69 @@ FileUtils.cp_r("dev/docs/images/plugins", ".vuepress/public/")
 
 puts ">>> Migrating plugins: Audioengines"
 
-Dir.glob("dev/docs/plugins/_plugins_audioengines/**") { |path|
+Dir.glob("dev/docs/NPE-Files/_plugins_audioengines/**") { |path|
     plugin = File.basename(path)
     puts " -> #{plugin}"
     FileUtils.mkdir_p("plugins/audioengines/" + plugin)
-    FileUtils.cp("dev/docs/plugins/_plugins_audioengines/" + plugin + "/readme.md", "plugins/audioengines/" + plugin)
+    FileUtils.cp("dev/docs/NPE-Files/_plugins_audioengines/" + plugin + "/readme.md", "plugins/audioengines/" + plugin)
 }
 
 puts ">>> Migrating plugins: Speechhandlers"
 
-Dir.glob("dev/docs/plugins/_plugins_speechhandlers/**") { |path|
+Dir.glob("dev/docs/NPE-Files/_plugins_speechhandlers/**") { |path|
     plugin = File.basename(path)
     puts " -> #{plugin}"
     FileUtils.mkdir_p("plugins/speechhandlers/" + plugin)
-    FileUtils.cp("dev/docs/plugins/_plugins_speechhandlers/" + plugin + "/readme.md", "plugins/speechhandlers/" + plugin)
+    FileUtils.cp("dev/docs/NPE-Files/_plugins_speechhandlers/" + plugin + "/readme.md", "plugins/speechhandlers/" + plugin)
 }
 
 puts ">>> Migrating plugins: Text to Intents"
 
-Dir.glob("dev/docs/plugins/_plugins_ttis/**") { |path|
+Dir.glob("dev/docs/NPE-Files/_plugins_ttis/**") { |path|
     plugin = File.basename(path)
     puts " -> #{plugin}"
     FileUtils.mkdir_p("plugins/ttis/" + plugin)
-    FileUtils.cp("dev/docs/plugins/_plugins_ttis/" + plugin + "/readme.md", "plugins/ttis/" + plugin)
+    FileUtils.cp("dev/docs/NPE-Files/_plugins_ttis/" + plugin + "/readme.md", "plugins/ttis/" + plugin)
 }
 
 puts ">>> Migrating plugins: Text to Speechs"
 
-Dir.glob("dev/docs/plugins/_plugins_ttss/**") { |path|
+Dir.glob("dev/docs/NPE-Files/_plugins_ttss/**") { |path|
     plugin = File.basename(path)
     puts " -> #{plugin}"
     FileUtils.mkdir_p("plugins/ttss/" + plugin)
-    FileUtils.cp("dev/docs/plugins/_plugins_ttss/" + plugin + "/readme.md", "plugins/ttss/" + plugin)
+    FileUtils.cp("dev/docs/NPE-Files/_plugins_ttss/" + plugin + "/readme.md", "plugins/ttss/" + plugin)
 }
 
 puts ">>> Migrating plugins: Speech to Texts"
 
-Dir.glob("dev/docs/plugins/_plugins_stts/**") { |path|
+Dir.glob("dev/docs/NPE-Files/_plugins_stts/**") { |path|
     plugin = File.basename(path)
     puts " -> #{plugin}"
     FileUtils.mkdir_p("plugins/stts/" + plugin)
-    FileUtils.cp("dev/docs/plugins/_plugins_stts/" + plugin + "/readme.md", "plugins/stts/" + plugin)
-}
-
-puts ">>> Migrating plugins: Speech to Text Trainers"
-
-Dir.glob("dev/docs/plugins/_plugins_stt_trainers/**") { |path|
-    plugin = File.basename(path)
-    puts " -> #{plugin}"
-    FileUtils.mkdir_p("plugins/stt_trainers/" + plugin)
-    FileUtils.cp("dev/docs/plugins/_plugins_stt_trainers/" + plugin + "/readme.md", "plugins/stt_trainers/" + plugin)
+    FileUtils.cp("dev/docs/NPE-Files/_plugins_stts/" + plugin + "/readme.md", "plugins/stts/" + plugin)
 }
 
 puts ">>> Migrating plugins: Voice Activation Detections"
 
-Dir.glob("dev/docs/plugins/_plugins_vads/**") { |path|
+Dir.glob("dev/docs/NPE-Files/_plugins_vads/**") { |path|
     plugin = File.basename(path)
     puts " -> #{plugin}"
     FileUtils.mkdir_p("plugins/vads/" + plugin)
-    FileUtils.cp("dev/docs/plugins/_plugins_vads/" + plugin + "/readme.md", "plugins/vads/" + plugin)
+    FileUtils.cp("dev/docs/NPE-Files/_plugins_vads/" + plugin + "/readme.md", "plugins/vads/" + plugin)
 }
 
 puts ">>> Migrating plugins: Visualizations"
 
-Dir.glob("dev/docs/plugins/_plugins_visualizations/**") { |path|
+Dir.glob("dev/docs/NPE-Files/_plugins_visualizations/**") { |path|
     plugin = File.basename(path)
     puts " -> #{plugin}"
     FileUtils.mkdir_p("plugins/visualizations/" + plugin)
-    FileUtils.cp("dev/docs/plugins/_plugins_visualizations/" + plugin + "/readme.md", "plugins/visualizations/" + plugin)
+    FileUtils.cp("dev/docs/NPE-Files/_plugins_visualizations/" + plugin + "/readme.md", "plugins/visualizations/" + plugin)
 }
 
 puts ">>> Writing plugin arrays to files for sidebar navigation"
-["audioengines", "speechhandlers", "ttis", "ttss", "stts", "stt_trainers", "vads", "visualizations"].each { |type|
+["audioengines", "speechhandlers", "ttis", "ttss", "stts", "vads", "visualizations"].each { |type|
     File.open(".vuepress/plugins-#{type}.js", "w+") { |file|
         file.puts "module.exports = ["
 
@@ -140,6 +161,3 @@ puts ">>> Writing plugin arrays to files for sidebar navigation"
         file.puts "]"
     }
 }
-
-#puts ">>> Deleting pre-migration docs"
-#FileUtils.rm_rf("dev/docs/plugins")
